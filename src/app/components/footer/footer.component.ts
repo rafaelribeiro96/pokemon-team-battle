f655a0aa060cd2e40a0daf3c526b3def38873c2e
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2025-05-04 08:33:35 -0300

    feat: aprimora o rodapé com novas seções, links sociais e informações de contato, além de melhorias no estilo

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
