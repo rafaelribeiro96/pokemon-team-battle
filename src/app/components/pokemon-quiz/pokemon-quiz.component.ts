3b10433e822f98847d50fb824e33ea4271b40cbb
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2025-05-05 19:39:14 -0300

    feat: adiciona comentários de cabeçalho nos arquivos do componente Pokémon Quiz

--- src/app/components/pokemon-quiz/pokemon-quiz.component.ts
+++ src/app/components/pokemon-quiz/pokemon-quiz.component.ts
@@ -1,3 +1,4 @@
+/* pokemon-quiz.component.ts */
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
