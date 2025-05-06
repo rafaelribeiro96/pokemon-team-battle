901e68fa2420d34614d4bdf56e715b5c3dda553c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2025-05-06 16:54:26 -0300

    feat: atualizar URL da API de produção para incluir o protocolo e o caminho correto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'pokemon-backend-production-af7c.up.railway.app',
+  apiUrl: 'https://pokemon-backend-production-af7c.up.railway.app/api',
 };
